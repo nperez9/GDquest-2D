948c95875cb656565f8cae63e71458bd17a0d00b
Nicolas Agustín Perez <nicolasperez95.a@gmail.com>
2025-09-11 02:04:13 -0300

    feat: added last chapter

--- M05.loot_it_all_workbook/lessons/item.gd
+++ M05.loot_it_all_workbook/lessons/item.gd
@@ -1,7 +1,18 @@
 extends Area2D
+@export var position_offset := Vector2(0.0, 4.0)
 
 func _ready() -> void:
 	area_entered.connect(_on_area_entered)
+	play_floating_animation()
 	
 func _on_area_entered(ship: Area2D) -> void:
 	queue_free()
+
+func play_floating_animation() -> void:
+	var tween := create_tween()
+	var duration := randf_range(0.7, 1.3)
+	
+	tween.set_trans(Tween.TRANS_SINE)
+	tween.tween_property($Sprite2D, "position", position_offset, duration)
+	tween.tween_property($Sprite2D, "position", position_offset * -1.0, duration)
+	tween.set_loops()
