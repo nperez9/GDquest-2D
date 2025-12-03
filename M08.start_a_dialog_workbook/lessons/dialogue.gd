3eb4d98ff1a10e513fae60e1e02758ba83a9184d
Nicolas <nicolasperez95.a@gmail.com>
2025-12-02 21:55:50 -0300

    continued

--- M08.start_a_dialog_workbook/lessons/dialogue.gd
+++ M08.start_a_dialog_workbook/lessons/dialogue.gd
@@ -52,27 +52,28 @@ var dialogue_items: Array[Dictionary] = [
 		"character": bodies["sophia"]
 	}
 ]
-var current_item_index := 0
 
 ## UI element that shows the texts
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
-## UI element that progresses to the next text
-@onready var next_button: Button = %NextButton
 ## Audio player that plays voice sounds while text is being written
 @onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
 ## The character
 @onready var body: TextureRect = %Body
 ## The Expression
 @onready var expression: TextureRect = %Expression
+@onready var action_buttons_v_box_container: VBoxContainer = %ActionButtonsVBoxContainer
 
 
 func _ready() -> void:
-	show_text()
-	next_button.pressed.connect(advance)
-
+	create_buttons({
+		"Vamo boca": 1,
+		"Eyes with no face": 3,
+		"other": 4,
+	})
+	show_text(0)
 
 ## Draws the current text to the rich text element
-func show_text() -> void:
+func show_text(current_item_index: int = 0) -> void:
 	# We retrieve the current item from the array
 	var current_item := dialogue_items[current_item_index]
 	# from the item, we extract the properties.
@@ -106,24 +107,6 @@ func show_text() -> void:
 	# We animate the character sliding in.
 	slide_in()
 
-	# Finally, we disable the next button until the text finishes displaying.
-	next_button.disabled = true
-	tween.finished.connect(func() -> void:
-		next_button.disabled = false
-	)
-
-
-## Progresses to the next slide.
-func advance() -> void:
-	# We increment the slide amount by 1
-	current_item_index += 1
-	if current_item_index == dialogue_items.size():
-		# if we reached the last slide, quit
-		get_tree().quit()
-	else:
-		# otherwise, show the text
-		show_text()
-
 
 ## Animates the character when they start talking
 func slide_in() -> void:
@@ -133,3 +116,18 @@ func slide_in() -> void:
 	slide_tween.tween_property(body, "position:x", 0, 0.3)
 	body.modulate.a = 0
 	slide_tween.parallel().tween_property(body, "modulate:a", 1, 0.2)
+
+func create_buttons(btn_dictorionary: Dictionary[String, int]) -> void:
+	for btn_key in btn_dictorionary:
+		var button = Button.new()
+		button.text = btn_key.capitalize()
+		button.pressed.connect(
+			func ():
+				show_text(btn_dictorionary[btn_key])
+				clear_btns()
+		)
+		action_buttons_v_box_container.add_child(button)
+
+func clear_btns() -> void:
+	for button in action_buttons_v_box_container.get_children():
+			button.queue_free()
