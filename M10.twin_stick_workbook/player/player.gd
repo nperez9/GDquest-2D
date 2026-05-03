b2b918dd0e3aad1730c80deb2af535d3206b04f2
Nicolas <nicolasperez95.a@gmail.com>
2026-05-03 14:36:09 -0300

    feat: added cool stuff

--- M10.twin_stick_workbook/player/player.gd
+++ M10.twin_stick_workbook/player/player.gd
@@ -1,40 +1,45 @@
 class_name Player extends CharacterBody2D
 
-
 const SPEED = 300.0
 const MAX_SPEED = 600.0
 const ACCLERATION = 1200.0
 const DEACCELERATION = 1300.0
-const FIRE_RATE = 2
+const FIRE_RATE = 0.15
 
 var can_fire := true
-var current_direction: Vector2
+var current_direction: Vector2 = Vector2.DOWN
 
 @onready var fire_rate := %FireRate
+var bullet = preload("res://weapons/bullet.tscn")
 
 func _ready() -> void:
 	print("aaaa")
 	fire_rate.wait_time = FIRE_RATE
 	fire_rate.connect("timeout", func(): can_fire = true)
 
-func _process(delta: float) -> void:
-	if (Input.is_action_pressed("fire") && can_fire):
-		shoot()
-
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("move-left", "move-right", "move-up", "move-down")
 	var has_input_direction := direction.length() > 0.0
 	if has_input_direction:
+		current_direction = direction
 		var desired_velocity = direction * MAX_SPEED
 		velocity = velocity.move_toward(desired_velocity, ACCLERATION * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, delta * DEACCELERATION)
 		
+	if (Input.is_action_pressed("fire") && can_fire):
+		shoot()
+
+		
 	move_and_slide()
 
 ## Fires a shoot
 func shoot():
-	print("kike fire")
+	var _bullet: BaseBullet = bullet.instantiate()
+	get_tree().current_scene.add_child(_bullet)
+	_bullet.global_position = global_position
+	print(current_direction)
+	_bullet.set_direction(current_direction)
 	fire_rate.start()
 	can_fire = false
 	print_debug("FIREEEEEEE")
--- M10.twin_stick_workbook/weapons/bullet.gd
+++ M10.twin_stick_workbook/weapons/bullet.gd
@@ -1,23 +1,28 @@
 class_name BaseBullet extends Area2D
 
-@export var speed := 800.0
-@export var distance := 4000
+@export var speed := 1200.0
+@export var max_range := 2000
+@export var spread := 7.0 # Degrees of random spread
 
-var direction := Vector2.RIGHT
-var asd := Vector2.ZERO
-var target_pos := Vector2.ZERO
+var direction := Vector2.ZERO
+var traveled_distance := 0.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	target_pos = direction.normalized() * distance
-
+func set_direction(dir: Vector2) -> void:
+	var random_rad = deg_to_rad(randf_range(-spread, spread))	
+	# 2. Apply rotation to the base direction and normalize
+	direction = dir.rotated(random_rad).normalized()
+	rotation = direction.angle()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var step = direction * speed * delta
+	global_position += step
 	
-	global_position = global_position.move_toward(target_pos, speed * delta)
+	# Track distance traveled instead of a fixed coordinate
+	traveled_distance += step.length()
 	
-	if global_position == target_pos:
-		queue_free()
+	if traveled_distance >= max_range:
+		_destroy()
 	
-	
+func _destroy() -> void:
+	queue_free()
