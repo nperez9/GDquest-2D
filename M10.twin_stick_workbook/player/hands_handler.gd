0827089933137875308a023209b89fe01b90c605
Nicolas <nicolasperez95.a@gmail.com>
2026-05-04 01:42:58 -0300

    feat: added colntrol behivioir

--- M10.twin_stick_workbook/player/hands_handler.gd
+++ M10.twin_stick_workbook/player/hands_handler.gd
@@ -1,8 +1,22 @@
 class_name HandsHandler extends Node2D
 
+var is_using_gamepad := false
+var aim_direction := Vector2.ZERO
+
+## Coll stuff to handle controller or mouse in live
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion or event is InputEventKey:
+		is_using_gamepad = false
+	elif event is InputEventJoypadButton or event is InputEventJoypadMotion:
+		is_using_gamepad = true
+
+
 func _process(delta: float) -> void:
-	var mouse = get_global_mouse_position()
-	var direction_to_mouse = global_position.direction_to(mouse)
-	rotation = direction_to_mouse.angle()
-	%Weapon.set_direction(direction_to_mouse)
+	if is_using_gamepad:
+		aim_direction = Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down")
+	else:
+		var mouse_pos = get_global_mouse_position()
+		aim_direction = global_position.direction_to(mouse_pos)
+	rotation = aim_direction.angle()
+	%Weapon.set_direction(aim_direction)
 	
