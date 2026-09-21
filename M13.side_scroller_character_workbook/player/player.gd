6c87096e9161f69379be1828335c53a1f40a98d8
Nicolas Perez <nicolasperez95.a@gmail.com>
2026-09-21 18:36:58 -0300

    feat: added stuff

--- M13.side_scroller_character_workbook/player/player.gd
+++ M13.side_scroller_character_workbook/player/player.gd
@@ -1,16 +1,43 @@
+
 class_name Player extends CharacterBody2D
 
 @export var acceleration := 500.0
 @export var deceleration := 1400.0
+@export var air_acceleration := 500.0
 @export var max_speed := 120.0
 @export var jump_gravity := 1200.0
 @export var max_gravity := 9800.0
- 
+@export var jump_force := 380.0
+
 @onready var _animated_sprite_2d: AnimatedSprite2D = %AnimatedSprite2D
 
-func _physics_process(delta: float) -> void:
-	var direction_x := signf(Input.get_axis("move_left", "move_right"))
+## First approach to state machines
+enum State {
+	GROUND,
+	JUMP,
+	FALL
+}
 
+var direction_x := 0.0
+
+var _current_state: State = State.GROUND
+
+func _ready() -> void:
+	_transtition_to_state(_current_state)
+
+func _physics_process(delta: float) -> void:
+	direction_x = signf(Input.get_axis("move_left", "move_right"))
+	
+	match _current_state:
+		State.GROUND:
+			process_ground_state(delta)
+		State.JUMP:
+			process_jump_state(delta)
+
+	velocity.y += clampf(jump_gravity * delta, velocity.y, max_gravity)
+	move_and_slide()
+	
+func process_ground_state(delta: float):
 	var is_moving := absf(direction_x) > 0.0
 	if is_moving:
 		## this not pass when its zero
@@ -21,6 +48,30 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, deceleration * delta)
 		_animated_sprite_2d.play("idle")
-	print_debug(jump_gravity)
-	velocity.y += clampf(jump_gravity * delta, velocity.y, max_gravity)
-	move_and_slide()
+	
+	if Input.is_action_just_pressed("jump"):
+		_transtition_to_state(State.JUMP)
+		
+func process_jump_state(delta: float):
+	if direction_x != 0:
+		velocity.x += air_acceleration * direction_x * delta
+		velocity.x = clampf(velocity.x, -max_speed, max_speed)
+		_animated_sprite_2d.flip_h = direction_x < 0.0
+	else:
+		velocity.x = 0
+	
+	
+func _transtition_to_state(new_state: State) -> void:
+	var previous_state := _current_state
+	_current_state = new_state
+	
+	## exit current state. can add things on exit that state EX: sounds, vfx
+	match previous_state: 
+		pass
+		
+	# Enter new state the same, can add things on transitio
+	match _current_state:
+		State.JUMP:
+			velocity.y = -1 * jump_force
+			_animated_sprite_2d.play("jump")
+	
